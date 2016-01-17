99fd77ec95e6b4243f88ed3d4366a29c71206180
Robert J Samson <rjsamson@me.com>
2016-01-17 15:40:38 -0500

    Switch venue registry to use ets

--- lib/venue_registry.ex
+++ lib/venue_registry.ex
@@ -3,42 +3,42 @@ defmodule Stackfooter.VenueRegistry do
   use GenServer
 
   def start_link(name) do
-    GenServer.start_link(__MODULE__, :ok, name: name)
+    GenServer.start_link(__MODULE__, name, name: name)
   end
 
-  def lookup(pid, name) do
-    GenServer.call(pid, {:lookup, name})
+  def lookup(pid, name) when is_atom(pid) do
+    case :ets.lookup(pid, name) do
+      [{^name, venue}] -> {:ok, venue}
+      [] -> :error
+    end
   end
 
   def create(pid, name, tickers) do
     GenServer.cast(pid, {:create, name, tickers})
   end
 
-  def init(:ok) do
-    venue_names = %{}
+  def init(table) do
+    venue_names = :ets.new(table, [:named_table, read_concurrency: true])
     refs = %{}
     {:ok, {venue_names, refs}}
   end
 
-  def handle_call({:lookup, name}, _from, {venue_names, _} = state) do
-    {:reply, Map.fetch(venue_names, name), state}
-  end
-
   def handle_cast({:create, name, tickers}, {venue_names, refs}) do
-    if Map.has_key?(venue_names, name) do
-      {:noreply, venue_names}
-    else
-      {:ok, pid} = Supervisor.start_child(Stackfooter.Venue.Supervisor, [String.upcase(name), tickers])
-      ref = Process.monitor(pid)
-      refs = Map.put(refs, ref, name)
-      venue_names = Map.put(venue_names, name, pid)
-      {:noreply, {venue_names, refs}}
+    case lookup(venue_names, name) do
+      {:ok, _pid} ->
+        {:noreply, {venue_names, refs}}
+      :error ->
+        {:ok, pid} = Supervisor.start_child(Stackfooter.Venue.Supervisor, [String.upcase(name), tickers])
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(venue_names, {name, pid})
+        {:noreply, {venue_names, refs}}
     end
   end
 
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {venue_names, refs}) do
     {name, refs} = Map.pop(refs, ref)
-    venue_names = Map.delete(venue_names, name)
+    :ets.delete(venue_names, name)
     {:noreply, {venue_names, refs}}
   end
 

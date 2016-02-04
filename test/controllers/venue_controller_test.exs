41fb49d5a75fce095f4defa367db948b7eaadff4
Robert J Samson <rjsamson@me.com>
2016-02-03 23:35:41 -0500

    Add tests for stock quote and order cancellation

--- test/controllers/venue_controller_test.exs
+++ test/controllers/venue_controller_test.exs
@@ -5,7 +5,7 @@ defmodule Stackfooter.VenueControllerTest do
 
   @apikey "4cy7uf63Lw2Sx6652YmLwBKy662weU4q"
 
-  test "place order with no content type", %{conn: conn} do
+  test "place order with various content types", %{conn: conn} do
     {:ok, venue} = VenueRegistry.lookup(Stackfooter.VenueRegistry, "OBEX")
     Venue.reset(venue)
 
@@ -26,6 +26,114 @@ defmodule Stackfooter.VenueControllerTest do
     assert resp_direction == "sell"
     assert resp_fills == []
     assert resp_qty == 100
+
+    conn = put_req_header(conn(), "x-starfighter-authorization", @apikey)
+    |> put_req_header("content-type", "application/json")
+    |> post("/ob/api/venues/obex/stocks/nyc/orders", Poison.encode!(order))
+    resp = json_response(conn, 200)
+    assert resp
+    %{"ok" => resp_ok, "direction" => resp_direction, "fills" => resp_fills, "qty" => resp_qty} = resp
+    assert resp_ok
+    assert resp_direction == "sell"
+    assert resp_fills == []
+    assert resp_qty == 100
+  end
+
+  test "stock quote and order cancellation" do
+    {:ok, venue} = VenueRegistry.lookup(Stackfooter.VenueRegistry, "OBEX")
+    Venue.reset(venue)
+
+    Enum.each(4200..4210, fn x ->
+      Venue.place_order(venue, %{direction: "buy", symbol: "NYC", qty: 7, price: x, account: "admin", orderType: "limit"})
+    end)
+
+    Enum.each(4220..4230, fn x ->
+      Venue.place_order(venue, %{direction: "sell", symbol: "NYC", qty: 7, price: x, account: "admin", orderType: "limit"})
+    end)
+
+    Venue.place_order(venue, %{direction: "sell", symbol: "NYC", qty: 5, price: 0, account: "admin", orderType: "market"})
+
+    stock_quote = %{"ask" => 4220,
+    "askDepth" => 77,
+    "askSize" => 7,
+    "bid" => 4210,
+    "bidDepth" => 72,
+    "bidSize" => 2,
+    "last" => 4210,
+    "lastSize" => 5,
+    "ok" => true,
+    "symbol" => "NYC",
+    "venue" => "OBEX"}
+
+    conn = put_req_header(conn(), "x-starfighter-authorization", @apikey)
+    |> get("/ob/api/venues/obex/stocks/nyc/quote")
+    resp = json_response(conn, 200)
+
+    %{"ok" => resp_ok, "ask" => ask, "askDepth" => ask_depth, "askSize" => ask_size, "bid" => bid, "bidDepth" => bid_depth, "bidSize" => bid_size, "last" => last, "lastSize" => last_size} = resp
+
+    assert resp
+    assert resp_ok
+    assert ask == stock_quote["ask"]
+    assert ask_depth == stock_quote["askDepth"]
+    assert ask_size == stock_quote["askSize"]
+    assert bid == stock_quote["bid"]
+    assert bid_depth == stock_quote["bidDepth"]
+    assert bid_size == stock_quote["bidSize"]
+    assert last == stock_quote["last"]
+    assert last_size == stock_quote["lastSize"]
+
+    deleted_order = %{"account" => "RJSAMSON",
+    "direction" => "buy",
+    "id" => 10,
+    "ok" => true,
+    "open" => false,
+    "orderType" => "limit",
+    "originalQty" => 7,
+    "price" => 4210,
+    "qty" => 2,
+    "symbol" => "NYC",
+    "totalFilled" => 5,
+    "venue" => "OBEX"}
+
+    conn = put_req_header(conn(), "x-starfighter-authorization", @apikey)
+    |> delete("/ob/api/venues/obex/stocks/nyc/orders/10")
+    resp = json_response(conn, 200)
+    %{"ok" => resp_ok, "id" => resp_id, "open" => resp_open, "qty" => resp_qty, "originalQty" => resp_original_qty} = resp
+
+    assert resp
+    assert resp_ok
+    assert resp_id == deleted_order["id"]
+    assert resp_open == deleted_order["open"]
+    assert resp_qty == deleted_order["qty"]
+    assert resp_original_qty == deleted_order["originalQty"]
+
+    updated_stock_quote = %{"ask" => 4220,
+    "askDepth" => 77,
+    "askSize" => 7,
+    "bid" => 4210,
+    "bidDepth" => 70,
+    "bidSize" => 0,
+    "last" => 4210,
+    "lastSize" => 5,
+    "ok" => true,
+    "symbol" => "NYC",
+    "venue" => "OBEX"}
+
+    conn = put_req_header(conn(), "x-starfighter-authorization", @apikey)
+    |> get("/ob/api/venues/obex/stocks/nyc/quote")
+    resp = json_response(conn, 200)
+    %{"ok" => resp_ok, "ask" => ask, "askDepth" => ask_depth, "askSize" => ask_size, "bid" => bid, "bidDepth" => bid_depth, "bidSize" => bid_size, "last" => last, "lastSize" => last_size} = resp
+
+    assert resp
+    assert resp_ok
+    assert ask == updated_stock_quote["ask"]
+    assert ask_depth == updated_stock_quote["askDepth"]
+    assert ask_size == updated_stock_quote["askSize"]
+    assert bid == updated_stock_quote["bid"]
+    assert bid_depth == updated_stock_quote["bidDepth"]
+    assert bid_size == updated_stock_quote["bidSize"]
+    assert last == updated_stock_quote["last"]
+    assert last_size == updated_stock_quote["lastSize"]
   end
 
   test "all responses reply with ok", %{conn: conn} do

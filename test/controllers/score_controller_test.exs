9ad47d29aad4d2b66d4221b131e24782207d35b2
Robert J Samson <rjsamson@me.com>
2016-02-11 09:55:51 -0500

    Additional tests for score_controller

--- test/controllers/score_controller_test.exs
+++ test/controllers/score_controller_test.exs
@@ -24,18 +24,42 @@ defmodule Stackfooter.ScoreControllerTest do
     end)
 
     Enum.each(4200..4210, fn x ->
-      Venue.place_order(venue, %{direction: "sell", symbol: "NYC", qty: 7, price: x, account: "account2", orderType: "limit"})
+      Venue.place_order(venue, %{direction: "sell", symbol: "NYC", qty: 7, price: x, account: "rjsamson", orderType: "limit"})
       :timer.sleep(20)
-      Venue.place_order(venue, %{direction: "sell", symbol: "NYC", qty: 7, price: x, account: "account2", orderType: "limit"})
+      Venue.place_order(venue, %{direction: "sell", symbol: "NYC", qty: 7, price: x, account: "rjsamson", orderType: "limit"})
     end)
 
     Enum.each(4220..4230, fn x ->
-      Venue.place_order(venue, %{direction: "buy", symbol: "NYC", qty: 7, price: x, account: "account2", orderType: "limit"})
+      Venue.place_order(venue, %{direction: "buy", symbol: "NYC", qty: 7, price: x, account: "rjsamson", orderType: "limit"})
       :timer.sleep(20)
-      Venue.place_order(venue, %{direction: "buy", symbol: "NYC", qty: 7, price: x, account: "account2", orderType: "limit"})
+      Venue.place_order(venue, %{direction: "buy", symbol: "NYC", qty: 7, price: x, account: "rjsamson", orderType: "limit"})
     end)
 
     :ok
+  end
+
+  test "returns the correct score for all users" do
+    scores = [%{"cash" => 1176, "name" => "A", "nav" => 1176, "positions" => [%{"price" => 4225, "qty" => 0, "stock" => "NYC"}]},
+              %{"cash" => -1176, "name" => "RJSA", "nav" => -1176, "positions" => [%{"price" => 4225, "qty" => 0, "stock" => "NYC"}]}]
+
+    conn = get(conn(), "/ob/api/scores")
+    resp = json_response(conn, 200)
+
+    assert resp
+    assert resp["ok"]
+    assert resp["scores"] == scores
+  end
+
+  test "returns the correct score for an individual user" do
+    conn = put_req_header(conn(), "x-starfighter-authorization", @admin_apikey)
+    |> get("/ob/api/scores/admin")
+    resp = json_response(conn, 200)
+
+    account_score = %{"cash" => 1176, "name" => "ADMIN", "nav" => 1176, "positions" => [%{"price" => 4225, "qty" => 0, "stock" => "NYC"}]}
+
+    assert resp
+    assert resp["ok"]
+    assert resp["account"] == account_score
   end
 
   test "all scores route is unauthenticated" do

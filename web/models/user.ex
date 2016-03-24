c6aeff526866a26e379651b20af450743c566bc3
Robert J Samson <rjsamson@me.com>
2016-03-23 22:02:09 -0400

    Add user

---
+++ web/models/user.ex
@@ -0,0 +1,36 @@
+defmodule Stackfooter.User do
+  use Stackfooter.Web, :model
+
+  schema "users" do
+    field :username, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    field :api_keys, {:array, :string}
+
+    timestamps
+  end
+
+  @required_fields ~w(username password_hash api_keys)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+end

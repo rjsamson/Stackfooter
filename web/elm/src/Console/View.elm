f04448cb02d019b0e67975b427742d87c9950936
Robert J Samson <rjsamson@me.com>
2016-03-28 12:02:47 -0400

    Format date

--- web/elm/src/Console/View.elm
+++ web/elm/src/Console/View.elm
@@ -5,6 +5,8 @@ import Html.Attributes exposing (href, class)
 import Console.Actions exposing (..)
 import Console.Models exposing (..)
 import String
+import Date exposing (..)
+import Result
 
 view : Signal.Address Action -> Model -> Html.Html
 view address model =
@@ -29,10 +31,11 @@ tickerTable address model =
         [ th [] [ text "Symbol" ]
         , th [] [ text "Venue" ]
         , th [] [ text "Bid" ]
+        , th [] [ text "Bid Size/Depth" ]
         , th [] [ text "Ask" ]
         , th [] [ text "Last" ]
-        , th [] [ text "Bid Size/Depth" ]
         , th [] [ text "Ask Size/Depth" ]
+        , th [] [ text "Last Trade" ]
         ]
       ]
     , tbody
@@ -42,10 +45,11 @@ tickerTable address model =
         [ td [] [ text model.symbol ]
         , td [] [ text model.venue ]
         , td [] [ text (formatPrice model.bid) ]
+        , td [] [ text ((toString model.bidSize) ++ " / " ++ (toString model.bidDepth)) ]
         , td [] [ text (formatPrice model.ask) ]
         , td [] [ text (formatPrice model.last) ]
-        , td [] [ text ((toString model.bidSize) ++ " / " ++ (toString model.bidDepth)) ]
         , td [] [ text ((toString model.askSize) ++ " / " ++ (toString model.askDepth)) ]
+        , td [] [ text (formatDate model.lastTrade) ]
         ]
       ]
     ]
@@ -58,3 +62,11 @@ formatPrice price =
     dollars =  if price < 100 then "0" else String.slice 0 -2 priceStr
   in
     "$" ++ dollars ++ "." ++ cents
+
+formatDate : String -> String
+formatDate date =
+  case Date.fromString date of
+    Ok d ->
+      toString (Date.hour d) ++ ":" ++ toString (Date.minute d) ++ ":" ++ toString (Date.second d) ++ ":" ++ toString (Date.millisecond d)
+    Err err ->
+      ""

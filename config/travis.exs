6602809f9b804503225d643cc6d43acfe0f1bb78
Robert J Samson <rjsamson@me.com>
2016-06-23 15:25:43 -0400

    Fix travis DB

---
+++ config/travis.exs
@@ -0,0 +1,23 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :stackfooter, Stackfooter.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :stackfooter, Stackfooter.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "stackfooter_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+config :stackfooter, :bootstrap,
+  default_api_key: "4cy7uf63Lw2Sx6652YmLwBKy662weU4q",
+  default_account: "admin"

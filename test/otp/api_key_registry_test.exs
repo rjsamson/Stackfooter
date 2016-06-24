09925e3fc6006ebfe6c1e1700a4dcb00efc96045
Robert J Samson <rjsamson@me.com>
2016-06-24 11:39:00 -0400

    Add API Key reset API

--- test/otp/api_key_registry_test.exs
+++ test/otp/api_key_registry_test.exs
@@ -3,6 +3,7 @@ defmodule Stackfooter.ApiKeyRegistryTest do
   alias Stackfooter.ApiKeyRegistry
 
   setup do
+    Stackfooter.ApiKeyRegistry.reset_api_keys(Stackfooter.ApiKeyRegistry)
     Stackfooter.ApiKeyRegistry.add_key(Stackfooter.ApiKeyRegistry, "4cy7uf63Lw2Sx6652YmLwBKy662weU4q", "admin")
     Stackfooter.ApiKeyRegistry.add_key(Stackfooter.ApiKeyRegistry, "7eWeGhc8n0va5bjT66C0Vl1fBw2618BJ", "rjsamson")
 
@@ -13,5 +14,12 @@ defmodule Stackfooter.ApiKeyRegistryTest do
     all_accounts = ApiKeyRegistry.all_account_names(ApiKeyRegistry)
 
     assert all_accounts == ["RJSAMSON", "ADMIN"]
+  end
+
+  test "resets all API keys" do
+    Stackfooter.ApiKeyRegistry.reset_api_keys(Stackfooter.ApiKeyRegistry)
+    all_accounts = ApiKeyRegistry.all_account_names(ApiKeyRegistry)
+
+    assert all_accounts == []
   end
 end
